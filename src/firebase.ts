a0747b7752819003d125c8042d72a0ef58dfb493
Dipesh Nayak <dipeshnayak@outlook.com>
2021-11-18 20:00:23 +0530

    heroku env vars

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp:any = firebase.initializeApp(firebaseConfig);
